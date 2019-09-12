58f578e0241043530db3c3d1b13c5e3bcde238aa
Gaurav Maheshwari <saist1993@gmail.com>
2019-09-12 16:55:22 +0200

    argparse support

---
+++ lilbert/gpu1.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=1 python run_glue_custom.py --dataset SST-2
+CUDA_VISIBLE_DEVICES=1 python run_glue_custom.py --dataset MRPC
+CUDA_VISIBLE_DEVICES=1 python run_glue_custom.py --dataset SST-B
+CUDA_VISIBLE_DEVICES=1 python run_glue_custom.py --dataset MNLI
+CUDA_VISIBLE_DEVICES=1 python run_glue_custom.py --dataset QNLI
