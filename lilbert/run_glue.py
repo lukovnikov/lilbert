44d04934c69dc61cda4a7d40a63ea2e7ab76ade2
lukovnikov <lukovnikov@outlook.com>
2019-09-12 17:21:23 +0200

    normal glue stuff

--- lilbert/run_glue.py
+++ lilbert/run_glue.py
@@ -394,6 +394,7 @@ def main():
     # Setup CUDA, GPU & distributed training
     if args.gpu != -1:
         device = torch.device("cuda", args.gpu)
+        args.n_gpu = 1
     elif args.local_rank == -1 or args.no_cuda:
         device = torch.device("cuda" if torch.cuda.is_available() and not args.no_cuda else "cpu")
         args.n_gpu = torch.cuda.device_count()
