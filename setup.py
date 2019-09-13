d5206e573d378d08993f14f72a707791633a21fb
lukovnikov <lukovnikov@outlook.com>
2019-09-13 17:02:27 +0200

    custom glue scripts for neuron prune

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name="lilbert",
+      description="lilbert",
+      author="Sum-Ting Wong",
+      author_email="sumting@wo.ng",
+      install_requires=[],
+      packages=["lilbert"],
+      )
